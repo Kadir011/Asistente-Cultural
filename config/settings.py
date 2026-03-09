7d5fd4d2637e6d51b14c5c2d34df1c84b57c3ea9
barquetbravokadir@gmail.com <barquetbravokadir@gmail.com>
2026-03-08 20:45:49 -0500

    Fix: Update email host user for contact form

--- config/settings.py
+++ config/settings.py
@@ -171,9 +171,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='kbarquetb@unemi.edu.ec')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='barquetbravokadir@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='') 
-
-
-
-
